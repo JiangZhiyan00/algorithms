17b895fb892e5edf91a815ea747adedaddc3c97b
JZY <jaemusic@foxmail.com>
2022-07-26 14:18:09 +0800

    单元测试结果注释下

--- data-structure/linkedList/src/test/java/com/jiangzhiyan/algorithms/test/ApiTest.java
+++ data-structure/linkedList/src/test/java/com/jiangzhiyan/algorithms/test/ApiTest.java
@@ -9,15 +9,14 @@ public class ApiTest {
     @Test
     public void testLinkedList(){
         List<String> list = new LinkedList<>();
-        System.out.println(list.size());
+        System.out.println(list.size());//0
         list.add("aaa");
         list.addLast("bbb");
         list.addFirst("111");
-        System.out.println(list.get(1));
-        list.printLinkList();
-
+        System.out.println(list.get(1));//aaa
+        list.printLinkList();//目前的列表，头节点：111 尾节点：bbb 整体：111,aaa,bbb
         list.remove("111");
-        System.out.println(list.size());
-        list.printLinkList();
+        System.out.println(list.size());//2
+        list.printLinkList();//目前的列表，头节点：aaa 尾节点：bbb 整体：aaa,bbb
     }
 }
